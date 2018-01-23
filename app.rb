7ee09a435da343f165832a99e0b9d00576af96cd
Arda Ozcan <ardaoezcan@gmail.com>
2018-01-23 05:40:52 +0100

    included some files to test

--- app.rb
+++ app.rb
@@ -2,15 +2,43 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  'Very very nice'
-  'too nice maybe'
-  'is this possible?'
-  'if so, it is realy really nice'
-  'so it will return the very last line, which is this one :)'
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team' do 
+  erb :team
+end
+#get '/about/me' do
+#  erb :about_me
+#end
+#
+#get '/about/team' do
+#  erb :about_team
+#end
+
+
+get '/about/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  erb :"#{params['name']}"
+end
+
+get '/team/:member' do |n|
+
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  # n stores params['name']
+  "Hello #{n}!"
 end
